79276a1baad2ca93a83343fe97ec89720306a5ac
Gabriela Castro Santamaría <69209287+gabriela-castro-s@users.noreply.github.com>
2022-11-29 11:54:46 -0500

    Update ReserveServicesTest.java

--- src/test/java/edu/eci/cvds/gestor/services/ReserveServicesTest.java
+++ src/test/java/edu/eci/cvds/gestor/services/ReserveServicesTest.java
@@ -15,54 +15,5 @@ import java.util.List;
 
 public class ReserveServicesTest {
 
-    ReserveServices reserveServices;
-    GestorServices gestorServices;
-    int resource = 3;
-    int carnet = 1234567;
-    String status = "activa";
-    Date dateSql;
-
-    public ReserveServicesTest(){
-        reserveServices = ResourceFactory.getInstance().getReserveServices();
-        gestorServices = ResourceFactory.getInstance().getGestorServices();
-    }
-
-    @Before
-    public void setUp() throws ParseException {
-        java.util.Date date = new SimpleDateFormat("yyyy/MM/dd").parse("2022/05/04");
-        dateSql= new Date(date.getTime());
-    }
-
-    @Test
-    public void shouldReserve() throws ServicesException {
-        int reservationAmount = gestorServices.consultReservations().size();
-        reserveServices.reserveResource("2022/05/04","3:00","5:00",3,carnet,RecurrenceOptions.ONE_TIME,null,"cancelada");
-        Assert.assertEquals(reservationAmount+1, gestorServices.consultReservations().size());
-    }
-
-    @Test
-    public void ShouldCreateDailyReserves() throws ServicesException, ParseException {
-        int reservationAmount = gestorServices.consultReservations().size();
-        java.util.Date date = new SimpleDateFormat("yyyy/MM/dd").parse("2022/05/6");
-        reserveServices.reserveResource("2022/05/04","3:00","5:00",4,carnet,RecurrenceOptions.DAILY,date,"cancelada");
-        Assert.assertEquals(reservationAmount+3,gestorServices.consultReservations().size());
-
-    }
-
-    @Test
-    public void ShouldCreateWeeklyReserves() throws ServicesException, ParseException {
-        int reservationAmount = gestorServices.consultReservations().size();
-        java.util.Date date = new SimpleDateFormat("yyyy/MM/dd").parse("2022/05/25");
-        reserveServices.reserveResource("2022/05/04","3:00","5:00",4,carnet,RecurrenceOptions.WEEKLY,date,"cancelada");
-        Assert.assertEquals(reservationAmount+4,gestorServices.consultReservations().size());
-
-    }
-    @Test
-    public void ShouldCreateMonthlyReserves() throws ServicesException, ParseException {
-        int reservationAmount = gestorServices.consultReservations().size();
-        java.util.Date date = new SimpleDateFormat("yyyy/MM/dd").parse("2022/08/04");
-        reserveServices.reserveResource("2022/05/04","3:00","5:00",4,carnet,RecurrenceOptions.MONTHLY,date,"cancelada");
-        Assert.assertEquals(reservationAmount+4,gestorServices.consultReservations().size());
-
-    }
+    
 }
